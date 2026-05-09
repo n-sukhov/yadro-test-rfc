c94505762256a1ececbd7249df4988ea63d97759
Nick <nsukhov22@gmail.com>
2026-05-09 15:36:05 +0300

    signal_generator done

--- src/signal_generator.cpp
+++ src/signal_generator.cpp
@@ -1,14 +1,15 @@
 #include "signal_generator.h"
 #include <cmath>
+#include <cstddef>
 
 
 const double PI = 3.14159265358979323846;
 const double TWO_PI = 2.0 * PI;
 const double SAMPLE_RATE = 100.0;
 
-double reduce_phase(double x) {
-    // Приведение аргумента x к диапазону 
-    x = std::remainder(x, PI);
+static double reduce_phase(double x) {
+    // Приведение аргумента x к диапазону [-pi/2; pi/2]
+    x = std::remainder(x, TWO_PI);
     if (x > PI / 2.0) {
         x = PI - x;
     } else if (x < -PI / 2.0) {
@@ -19,6 +20,7 @@ double reduce_phase(double x) {
 
 double sine_taylor(double x, double eps) {
     /* Разложение синуса в ряд Тейлора
+
     x : аргумент функции
     eps : точность
     */
@@ -50,5 +52,27 @@ std::vector<double> generate_sine(
     double end_time,
     double eps
 ) {
+    /* Генерация массива отсчетов синусоидального сигнала
     
+    frequency : частота синусоидального сигнала, Гц
+    start_time : начальное время генерации сигнала, с
+    end_time : конечное время генерации сигнала, с
+    eps : точность вычисления синуса рядом Тейлора
+    */
+    std::vector<double> signal;
+    if (end_time <= start_time) {
+        return signal;
+    }
+
+    size_t sample_count = static_cast<std::size_t>(SAMPLE_RATE * (end_time - start_time));
+    signal.reserve(sample_count);
+
+    for (size_t n = 0; n < sample_count; ++n) {
+        const double x_reduced = reduce_phase(
+            TWO_PI * frequency * (start_time + static_cast<double>(n) / SAMPLE_RATE)
+        );
+        signal.push_back(sine_taylor(x_reduced, eps));
+    }
+
+    return signal;
 }
